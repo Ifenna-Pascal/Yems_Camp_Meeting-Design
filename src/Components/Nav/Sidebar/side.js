6abc1578ae3e5c18c32760883eaa15a1b168e1ca
Ifenna-Pascal <71331395+Ifenna-Pascal@users.noreply.github.com>
2021-11-12 11:45:18 +0100

    finished the responsive navigation

--- src/Components/Nav/Sidebar/side.js
+++ src/Components/Nav/Sidebar/side.js
@@ -1,25 +1,25 @@
-import styled from "styled-components";
-import { Link as LinkS } from "react-scroll";
-import { ButtonContainer , Button} from '../style';
+import styled from 'styled-components';
+import { Link as LinkS } from 'react-scroll';
+import { ButtonContainer, Button } from '../style';
 export const Nav = styled.div`
     background: #555;
-    position:fixed;
-    height:100vh;
-    width:100%;
-    z-index:999;
-    opacity: ${({open})=> (open ? "100%" : "0")};
-    top:${({open})=> (open ? "11%" : "-100%")};
-    transition:1s ease-in-out;
-    left:0;
-`
+    position: fixed;
+    height: 100vh;
+    width: 100%;
+    z-index: 999;
+    opacity: ${({ open }) => (open ? '100%' : '0')};
+    top: ${({ open }) => (open ? '10%' : '-100%')};
+    transition: 1s ease-in-out;
+    left: 0;
+`;
 
 export const NavLinks = styled.ul`
     display: flex;
     width: 100%;
-    padding: 1rem, 1,5rem;
+    padding: 1rem, 1, 5rem;
     flex-direction: column;
     margin: 3rem 2rem;
-`
+`;
 export const Link = styled(LinkS)`
     margin-bottom: 1rem;
     height: 5rem;
@@ -27,17 +27,17 @@ export const Link = styled(LinkS)`
     letter-spacing: 1.6px;
     color: white;
     font-size: 1.1rem;
-    width:78%;
+    width: 78%;
     border-bottom: 1px solid gray;
-`
+`;
 
 export const ButtonContainers = styled(ButtonContainer)`
     margin-top: 2.7rem;
-    width:78%;
-`
+    width: 78%;
+`;
 
 export const Buttons = styled(Button)`
-    width:100%;
+    width: 100%;
     letter-spacing: 3px;
-    font-size: 1.2rem
-`
+    font-size: 1.2rem;
+`;
--- src/Components/Nav/style.js
+++ src/Components/Nav/style.js
@@ -2,35 +2,34 @@ import styled from 'styled-components';
 import { Link as LinkS } from 'react-scroll';
 import px2vw from '../../Util/resize';
 import { FaBars, FaTimes } from 'react-icons/fa';
-import "../../App.css";
+import '../../App.css';
 
 export const NavContainer = styled.section`
     background: white;
-    box-shadow: ${({scroll})=> scroll ? "0 2px 2px -2px rgba(0,0,0,.2)" : "none"};
+    box-shadow: ${({ scroll }) => (scroll ? '0 2px 2px -2px rgba(0,0,0,.2)' : 'none')};
     border-bottom: 0.5px solid #ece5e5;
-    height:90px;
-    padding-bottom: 5rem;
+    height: fit-content;
+    padding-bottom: 0.5rem;
     position: sticky;
-    z-index:10;
+    z-index: 10;
     top: 0;
     width: 100%;
     @media (min-width: 768px) {
-        height: ${px2vw(80, 1440)};
+        height: fit-content;
         border-bottom: none;
         padding-bottom: 0;
     }
-    @media (max-width: 768px && min-width: 568) {
-        height: ${px2vw(1900, 1440)};
+    /* @media (max-width: 798px) {
+        height: ${px2vw(190, 1440)};
         border-bottom: none;
         padding-bottom: 0;
-    }
+    } */
 `;
 export const NavContent = styled.div`
     display: flex;
     justify-content: space-between;
-    padding: 1.5rem 3rem;
+    padding: 0.9rem 3rem;
     @media (min-width: 768px) {
-         
     }
 `;
 
@@ -63,8 +62,7 @@ export const NavLinks = styled.ul`
         display: flex;
         justify-content: space-around;
         margin-right: 2rem;
-      }
-
+    }
 `;
 
 export const Link = styled(LinkS)`
@@ -75,10 +73,9 @@ export const Link = styled(LinkS)`
     font-weight: 500;
     letter-spacing: 0.8px;
 
-    &:hover{
+    &:hover {
         color: #7ad03a;
         cursor: pointer;
-        
     }
 `;
 export const NavLeft = styled.div`
@@ -87,23 +84,22 @@ export const NavLeft = styled.div`
 export const Heading = styled.h1`
     font-size: 2rem;
     font-family: 'Roboto Slab', serif;
-    span{
+    span {
         color: #7ad03a;
     }
 `;
 export const ButtonContainer = styled.div`
-     @media (min-width: 768px) {
+    @media (min-width: 768px) {
         margin-left: 4rem;
         display: flex;
         flex-direction: column;
         justify-content: center;
         align-items: center;
         text-align: center;
-     }
-
+    }
 `;
 export const Button = styled.button`
-    padding: 0.8rem 3rem;
+    padding: 0.6rem 3rem;
     border: none;
     background: #7ad03a;
     font-size: 1rem;
@@ -111,8 +107,8 @@ export const Button = styled.button`
     font-weight: 600;
     letter-spacing: 1px;
     font-family: 'Lato', sans-serif;
-    
-    &:hover{
+
+    &:hover {
         cursor: pointer;
         transition: 1s ease-in;
         color: #7ad03a;
